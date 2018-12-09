2575b8e248a87e57f6dbc35ca358619a7c4c16ec
Gregory Goijaerts <crecketgaming@gmail.com>
2018-12-09 22:12:40 +0100

    Fix sandbox funds request

--- src/Modes/Interactive/Prompts/select_monetary_account_id.ts
+++ src/Modes/Interactive/Prompts/select_monetary_account_id.ts
@@ -4,10 +4,7 @@ const { Select } = require("enquirer");
 export default async accounts => {
     const prompt = new Select({
         message: "Which monetary account would you like to use?",
-        choices: accounts.map(account => {
-            const accountType = Object.keys(account)[0];
-            const accountInfo = account[accountType];
-
+        choices: accounts.map(accountInfo => {
             return {
                 message: `${accountInfo.id}: ${accountInfo.description} - ${accountInfo.balance.value}`,
                 value: accountInfo.id
