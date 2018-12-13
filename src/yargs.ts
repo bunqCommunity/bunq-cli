7c80580125895163a92f59827b11872d817c50f5
Gregory Goijaerts <ggoijaerts@zig.nl>
2018-12-13 13:50:12 +0100

    Add experimental script for zsh completion

--- src/yargs.ts
+++ src/yargs.ts
@@ -56,7 +56,6 @@ const yargsResult = ({ defaultSavePath, defaultOutputLocationPath }) => {
         .env("BUNQ_CLI")
         .help("help")
         .scriptName("bunq-cli")
-        .completion("completion")
         .usage(
             `Interactive mode: bunq-cli 
 Or use a command: bunq-cli <sub-command> [options]`
@@ -77,6 +76,31 @@ Or use a command: bunq-cli <sub-command> [options]`
             "Simplified way to call standard GET and LIST endpoints",
             EndpointUrlSubCommand("endpoint")
         )
+
+        .completion("completion")
+        .command("zsh-completion", "generate zsh completion script", yargs => {
+            process.stdout.write(`###-begin-bunq-cli-completions-###
+#
+# yargs command completion script
+#
+# Installation: bunq-cli zsh-completion >> ~/.zshrc
+#    or bunq-cli zsh-completion >> ~/.bash_profile on OSX.
+#
+_yargs_completions()
+{
+  local reply
+  local si=$IFS
+
+  # ask yargs to generate completions.
+  IFS=$'\\n' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" bunq-cli --get-yargs-completions "\${words[@]}"))
+  IFS=$si
+
+  _describe 'values' reply
+}
+compdef _yargs_completions bunq-cli
+###-end-bunq-cli-completions-###`);
+            process.exit();
+        })
 
         .describe({
             save: "Storage location for bunqJSClient data",
